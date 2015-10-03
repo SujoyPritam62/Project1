04939f88fe886c0f9f2697e96eb2e4440db03415
mishuk222 <mishuk1971@gmail.com>
2015-10-03 18:12:30 +0600

    added all the files

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+namespace Project1
+{
+    class Triangle
+    {
+        int height, width, length;
+        public Triangle()
+        { }
+        public Triangle(int height, int width, int length)
+        {
+            this.length = length;
+            this.height = height;
+            this.width = width;
+            Console.WriteLine(height);
+            Console.WriteLine(width);
+            Console.WriteLine(length);
+
+
+
+
+        }
+        public double area()
+        {
+
+
+            double a = .5 * length * width * height;
+            return a;
+
+
+        }
+
+
+
+
+    }
+    class Main1
+    {
+        public static void Main(string[] args)
+        {
+            Triangle T1 = new Triangle(1, 2, 3);
+            Console.WriteLine("Area: {0}", T1.area());
+            Console.ReadKey();
+
+
+
+
+        }
+
+    }
+
+
+
+
+}
